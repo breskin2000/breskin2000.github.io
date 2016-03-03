ac7791b97b5c37e6c27624d91f1b20297872da16
breskin2000 <jakubpiasecki67@gmail.com>
2016-03-03 18:53:50 +0100

    Launch

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var CurrentScene = 1;
+var Scenes = 1;
+var SceneChanged = false;
+
+function OnLoad() {
+    Scenes = document.getElementsByClassName("scene").length;
+
+    setInterval(function() {
+        if (!SceneChanged) {
+            var SceneToOpen = CurrentScene + 1;
+            if (SceneToOpen > Scenes) {SceneToOpen = 1;}
+            OpenScene(SceneToOpen);
+        } else {SceneChanged = false;}
+    }, 5000);
+}
+
+function OpenScene(scene) {
+    if (CurrentScene != scene) {
+        if (CurrentScene % 2 == 0)
+            document.getElementById("openscene" + CurrentScene).style.borderTopColor = "#004882";
+        else
+            document.getElementById("openscene" + CurrentScene).style.borderBottomColor = "#004882";
+        
+        if (scene % 2 == 0)
+            document.getElementById("openscene" + scene).style.borderTopColor = "#007cff";
+        else
+            document.getElementById("openscene" + scene).style.borderBottomColor = "#007cff";
+            
+        document.getElementById("scene" + CurrentScene).style.opacity = "0";
+        document.getElementById("scene" + scene).style.opacity = "1";
+            
+        CurrentScene = scene;
+        SceneChanged = true;
+    }
+}
