10d5f34b3f1a3803d7b47ffea8b5c5b315857969
Jakub Piasecki <jakubpiasecki67@outlook.com>
2018-03-20 21:51:04 +0100

    quiz2

--- quiz/script.js
+++ quiz/script.js
@@ -2,10 +2,25 @@ var CurrentQuestion = 1;
 var Result = 0;
 var TimerWorking = false;
 var QuestionQuantity = 12;
+var Solo = "A";
 
 window.onload = function()
 {
-    document.getElementById("q" + CurrentQuestion).style.opacity = "1";
+    document.getElementById("type").style.opacity = "1";
+}
+
+function begin(solo)
+{
+    document.getElementById("type").style.opacity = "0";
+
+    if (Solo == "A") {
+        Solo = solo;
+        setTimeout(function() {
+            
+            document.getElementById("type").style.display = "none";
+            document.getElementById("q1").style.opacity = "1";
+        }, 500);
+    }
 }
 
 function choose(question, clicked, correct, message)
@@ -49,22 +64,34 @@ function choose(question, clicked, correct, message)
 }
 
 function finish() {
-    if (Result > 6)
+    if (Solo)
     {
-        document.getElementById("finishf").style.opacity = "1";
-        document.getElementById("finishg").style.display = "none";
-        document.getElementById("finishdziennikarska").style.display = "none";
-    }
-    else if (Result <= 6 && Result > 3)
-    {
-        document.getElementById("finishg").style.opacity = "1";
-        document.getElementById("finishf").style.display = "none";
-        document.getElementById("finishdziennikarska").style.display = "none";
+        document.getElementById("finishgroup").style.display = "none";
+        if (Result > 6)
+        {
+            document.getElementById("resf").innerText = "Wynik: " + Result + "/12";
+            document.getElementById("finishf").style.opacity = "1";
+            document.getElementById("finishg").style.display = "none";
+            document.getElementById("finishdziennikarska").style.display = "none";
+        }
+        else if (Result <= 6 && Result > 3)
+        {
+            document.getElementById("resg").innerText = "Wynik: " + Result + "/12";
+            document.getElementById("finishg").style.opacity = "1";
+            document.getElementById("finishf").style.display = "none";
+            document.getElementById("finishdziennikarska").style.display = "none";
+        }
+        else
+        {
+            document.getElementById("resd").innerText = "Wynik: " + Result + "/12";
+            document.getElementById("finishdziennikarska").style.opacity = "1";
+            document.getElementById("finishf").style.display = "none";
+            document.getElementById("finishg").style.display = "none";
+        }
     }
     else
     {
-        document.getElementById("finishdziennikarska").style.opacity = "1";
-        document.getElementById("finishf").style.display = "none";
-        document.getElementById("finishg").style.display = "none";
+        document.getElementById("finishgroup").style.opacity = "1";
+        document.getElementById("finishgroup").innerHTML = "<div class=\"result\" style=\"margin-top: 15vh;\">Wynik: " + Result + "/12<br>Zapraszamy do klasy F!</div>";
     }
 }
